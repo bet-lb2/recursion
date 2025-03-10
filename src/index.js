8dd84fd97b8ee2b4622620602570765bb651be10
shota <55336324+bet-lb2@users.noreply.github.com>
2025-03-10 17:19:30 +0900

    implement fibonacci recursive function and merge sort algorithm

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-import "./styles.css";
+function fibs(num) {
+    let arr = [];
+    let prev = 0;
+    let next = 1;
+    let output;
+    for (let i = 0; i < num; i++) {
+        arr.push(prev);
+        output = prev + next;
+        prev = next;
+        next = output;
+    }
+    return arr;
+}
+
+// console.log(fibs(13));
+
+function fibsRec(n) {
+    return n === 1 ? [0] : n === 2 ? [0, 1] : [...fibsRec(n - 1), fibsRec(n - 1)[n - 2] + fibsRec(n - 1)[n - 3]];
+}
+
+// console.log(fibsRec(8));
+
+function mergeSort(arr) {
+    return arr.toSorted((a, b) => a - b);
+}
+
+// console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
